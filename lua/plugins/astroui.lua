14be8d1e6f5e100d5ebd44d1436aba4c0e4f942b
Mukherjee, Saumyajyoti <saumyajyoti.mukherjee@intel.com>
2024-05-13 21:49:33 +0530

    Status line minor change

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -11,14 +11,14 @@ return {
     -- change colorscheme
     colorscheme = "catppuccin", -- astrodark",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
-    highlights = {
-      init = { -- this table overrides highlights in all themes
-        -- Normal = { bg = "#000000" },
-      },
-      astrodark = { -- a table of overrides/changes when applying the astrotheme theme
-        -- Normal = { bg = "#000000" },
-      },
-    },
+    -- highlights = {
+    --   init = { -- this table overrides highlights in all themes
+    --     -- Normal = { bg = "#000000" },
+    --   },
+    --   astrodark = { -- a table of overrides/changes when applying the astrotheme theme
+    --     -- Normal = { bg = "#000000" },
+    --   },
+    -- },
     -- Icons can be configured throughout the interface
     icons = {
       -- configure the loading of the lsp in the status line
@@ -32,16 +32,43 @@ return {
       LSPLoading8 = "⠧",
       LSPLoading9 = "⠇",
       LSPLoading10 = "⠏",
+      VimIcon = "",
+      ScrollText = "",
     },
     status = { -- Configure characters used as separators for various elements
       separators = {
         none = { "", "" },
-        left = { "", "  " },
-        right = { "  ", "" },
+        left = { "", "" }, -- separator for the left side of the statusline
+        right = { "", " " }, -- separator for the right side of the statusline
+        -- left = { "", "  " },
+        -- right = { "  ", "" },
         center = { "  ", "  " },
-        tab = { "|", " " },
+        tab = { "|", "" },
         breadcrumbs = "  ", -- 
         path = "  ",
+      },
+      colors = function(hl)
+        local get_hlgroup = require("astroui").get_hlgroup
+        -- use helper function to get highlight group properties
+        local comment_fg = get_hlgroup("Comment").fg
+        -- hl.git_branch_fg = comment_fg
+        -- hl.git_added = comment_fg
+        -- hl.git_changed = comment_fg
+        -- hl.git_removed = comment_fg
+        -- hl.blank_bg = get_hlgroup("Folded").fg
+        hl.file_info_bg = get_hlgroup("Visual").bg
+        -- hl.nav_icon_bg = get_hlgroup("String").fg
+        -- hl.nav_fg = hl.nav_icon_bg
+        hl.folder_icon_bg = get_hlgroup("@comment.warning").bg
+        return hl
+      end,
+      -- attributes = {
+      --   mode = { bold = true },
+      -- },
+      icon_highlights = {
+        file_icon = {
+          statusline = false,
+        },
       },
     },
   },
--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,87 +1,12 @@
 return {
-  -- {
-  --   "nvim-lualine/lualine.nvim",
-  --   event = "VeryLazy",
-  --   opts = {
-  --     theme = "catppuccin",
-  --     sections = {
-  --       lualine_a = {
-  --         {
-  --           "datetime",
-  --           -- options: default, us, uk, iso, or your own format string ("%H:%M", etc..)
-  --           style = "default",
-  --         },
-  --       },
-  --     },
-  --   },
-  -- },
-  {
-    "AstroNvim/astroui",
-    ---@type AstroUIOpts
-    opts = {
-      -- add new user interface icon
-      icons = {
-        VimIcon = "",
-        ScrollText = "",
-        -- GitBranch = "",
-        -- GitAdd = "",
-        -- GitChange = "",
-        -- GitDelete = "",
-      },
-      -- modify variables used by heirline but not defined in the setup call directly
-      status = {
-        -- define the separators between each section
-        separators = {
-          left = { "", "" }, -- separator for the left side of the statusline
-          right = { "", " " }, -- separator for the right side of the statusline
-          tab = { " ", " " },
-        },
-        -- add new colors that can be used by heirline
-        colors = function(hl)
-          local get_hlgroup = require("astroui").get_hlgroup
-          -- use helper function to get highlight group properties
-          local comment_fg = get_hlgroup("Comment").fg
-          -- hl.git_branch_fg = comment_fg
-          -- hl.git_added = comment_fg
-          -- hl.git_changed = comment_fg
-          -- hl.git_removed = comment_fg
-          -- hl.blank_bg = get_hlgroup("Folded").fg
-          hl.file_info_bg = get_hlgroup("Visual").bg
-          -- hl.nav_icon_bg = get_hlgroup("String").fg
-          -- hl.nav_fg = hl.nav_icon_bg
-          hl.folder_icon_bg = get_hlgroup("@comment.warning").bg
-          return hl
-        end,
-        attributes = {
-          mode = { bold = true },
-        },
-        icon_highlights = {
-          file_icon = {
-            statusline = false,
-          },
-        },
-      },
-    },
-  },
   {
     "rebelot/heirline.nvim",
     optional = true,
     opts = function(_, opts)
-      -- local Lazy = {
-      --   condition = require("lazy.status").has_updates,
-      --   update = { "User", pattern = "LazyUpdate" },
-      --   provider = function() return "   " .. require("lazy.status").updates() .. " " end,
-      --   on_click = {
-      --     callback = function() require("lazy").update() end,
-      --     name = "update_plugins",
-      --   },
-      --   hl = { fg = "gray" },
-      -- }
-
       local status = require "astroui.status"
       opts.statusline = { -- statusline
         hl = { fg = "fg", bg = "bg" },
-        status.component.mode { mode_text = { padding = { left = 1, right = 1 } } },
+        status.component.mode { mode_text = { padding = { left = 0, right = 0 } } },
         status.component.git_branch(),
         -- status.component.file_info(),
         status.component.git_diff(),
